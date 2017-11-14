90838b8db91a2875f142b4190f240f3a9c5871df
palashhedau <palashhedau900@gmail.com>
2017-11-14 14:41:20 -0800

    Lisitng all products dynamically

---
+++ client/src/components/MyProductList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux' ; 
+
+
+
+class myProductList extends Component {
+  
+    constructor(props){
+      super(props);
+
+      
+    }
+
+
+    render() {
+
+      const displayNone = {
+        display : "none"
+      }
+      console.log("My current Products " , this.props.myProducts.length) ; 
+
+      return (
+         <li className="liAttr">
+                  <div className="row">
+                    <div className="divForImage col-lg-3 ">
+                      <a>
+                        <div >
+                          <img className="imageAttr" alt="" src={require("../assets/1.jpg")} />
+                        </div>
+                        
+                      </a>
+                    </div>
+                    <div className="DivForImageInfo col-lg-7 textColorBlack">
+                      {this.props.product.name } : {this.props.product.description} 
+                    </div>
+                     <div className="col-lg-2 textColorBlack">
+                       <button className="btn btn-info"><Link to="/setUpAuction/samsung">Put on Auction</Link></button>
+                    </div>
+                  </div>
+         </li>
+                
+                
+           
+      );
+    }
+  }
+
+function mapDispatchToProps(dispatch){
+    return {
+    
+    }
+  }
+
+  function mapStateToProps(state) {
+      return {
+        isAuthenticated : state.AuthReducer.isAuthenticated,
+        myProducts : state.ProductReducer.myProduct
+      };
+  }
+
+  export default connect(mapStateToProps,mapDispatchToProps)(myProductList) ; 
--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
 	
-	myProduct :  null 
+	myProduct :  [] 
 }
 
 
